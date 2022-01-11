c66d009b507fed3552b7d90415b9c54643bc2e2b
aulianar <arisky540@gmail.com>
2022-01-11 08:55:57 +0700

    first commit

---
+++ src/main/java/praktikumti/konversiSuhu/KonversiSuhuApplication.java
@@ -0,0 +1,13 @@
+package praktikumti.konversiSuhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonversiSuhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonversiSuhuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/praktikumti/konversiSuhu/KonversiSuhuApplicationTests.java
@@ -0,0 +1,13 @@
+package praktikumti.konversiSuhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonversiSuhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
